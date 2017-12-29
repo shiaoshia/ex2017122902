7664b757dc8f85053b8cd49015f0338c07c0d3b5
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2017-12-29 19:20:31 +0800

    layout練習

--- app/src/main/java/tw/com/shiaoshia/ex2017122902/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2017122902/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         //改成其它layout
-        setContentView(R.layout.layout2);
+        setContentView(R.layout.activity_main);
     }
 }
